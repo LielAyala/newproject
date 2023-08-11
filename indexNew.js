d11767623f5c4aa06f7605abfdd6e96448ea3653
ayala_table <ayala941356@gmail.com>
2023-08-11 13:50:12 +0300

    תיקון לקוד INDEX קבלת נתונים של אדם אחר ושל זוג

--- indexNew.js
+++ indexNew.js
@@ -3,14 +3,53 @@ const port = 5421;
 const app = express();
 app.use(express.json());
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+//-----------------------------------------------------------
+let AllData=[];
+
+
+app.get("/",(req, res) => {
+
+    res.sendFile("./view/spa_main.html" , {root:__dirname});
+});
+
+
+app.get("/List",(req, res) => {
+    //res.send(AllData).json();
+    res.send(AllData);
+
+});
+
+app.post("/Add",(req, res) => {
+    let line={};
+    line.name = req.body.name;
+    line.phone = req.body.phone;
+    line.pobox = req.body.pob;
+    AllData.push(line);
+    console.log(req.body);
+    res.send("Ready to Add EndPoint");
+});
+
+
+app.post("/Add2" ,(req, res)=>{
+    let line={};
+    line.name = req.body.name;
+    line.phone = req.body.phone;
+    line.pobox = req.body.pob;
+    AllData.push(line);
+    line={};//יוצר הקצאה חדשה וכך יאפשר לפרטים הבאים להיכנס ולא ידרסו את הקודמים
+    line.name = req.body.name2;ף
+    line.phone = req.body.phone2;
+    line.pobox = req.body.pob;
+    AllData.push(line);
+    res.send("Ready to Add EndPoint");
+});
+
 //-----------------------------------------------------------
 
 app.listen(port,()=>{
     console.log(`now listening on port${port}`);
 })
 
-//------------------------------------------------
-app.get("/",(req, res) => {
-
-    res.sendFile("./view/spa_main.html" , {root:__dirname});
-});
+//-----------------------------------------------------------
